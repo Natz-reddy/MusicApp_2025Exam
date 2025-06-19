b84860dd8280dce737c5b7db4aba8611a2f49d06
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-06-19 08:46:58 +0200

    adding in variables

--- app/src/main/java/com/example/musicapp_2025exam/MainActivity.kt
+++ app/src/main/java/com/example/musicapp_2025exam/MainActivity.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicapp_2025exam.ui.theme.MusicApp_2025ExamTheme
@@ -26,6 +28,16 @@ class MainActivity : ComponentActivity() {
             val artistList = remember { mutableStateListOf< String>() }
             val ratingList = remember { mutableStateListOf< String>() }
             val commentsList = remember { mutableStateListOf< String>() }
+
+            var songtitle by remember { mutableStateOf("") }
+            var artist by remember { mutableStateOf("") }
+            var rating by remember { mutableStateOf("") }
+            var comments by remember { mutableStateOf("") }
+            var errormessage by remember { mutableStateOf("") }
+
+
+
+
 
             }
         }
