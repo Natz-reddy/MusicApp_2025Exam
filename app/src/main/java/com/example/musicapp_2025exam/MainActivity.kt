c695e7d71de95a11b7a5fcbc70bcd713100ef4a3
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-06-19 08:44:31 +0200

    adding values to store array

--- app/src/main/java/com/example/musicapp_2025exam/MainActivity.kt
+++ app/src/main/java/com/example/musicapp_2025exam/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicapp_2025exam.ui.theme.MusicApp_2025ExamTheme
@@ -18,6 +21,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            val songtitleList = remember { mutableStateListOf< String>() }
+            val artistList = remember { mutableStateListOf< String>() }
+            val ratingList = remember { mutableStateListOf< String>() }
+            val commentsList = remember { mutableStateListOf< String>() }
 
             }
         }
