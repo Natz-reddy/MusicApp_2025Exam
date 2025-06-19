273ace6aa5f01a3bd59762a931c833f3070ccd55
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-06-19 08:52:46 +0200

    editing screen layout

--- app/src/main/java/com/example/musicapp_2025exam/MainActivity.kt
+++ app/src/main/java/com/example/musicapp_2025exam/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,8 +20,12 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.musicapp_2025exam.ui.theme.MusicApp_2025ExamTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +45,37 @@ class MainActivity : ComponentActivity() {
             var comments by remember { mutableStateOf("") }
             var errormessage by remember { mutableStateOf("") }
 
+            Column (
+                modifier = Modifier.fillMaxSize().padding(16.dp),verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ){
+                Text(text = "Music Playlist", fontSize = 20.sp, fontFamily = FontFamily.SansSerif )
+
+                Spacer(modifier = Modifier.height(10.dp))
+                OutlinedTextField(
+                    value = songtitle,
+                    onValueChange = { songtitle = it },
+                    placeholder = { Text("Item name") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
 
 
 
