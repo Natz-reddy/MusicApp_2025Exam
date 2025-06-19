fd64d08c2c4276eaebfd380d0cdbc8e54027211f
Natz-reddy <ST10488460@rcconnect.edu.za>
2025-06-19 10:05:30 +0200

    button to show songs

--- app/src/main/java/com/example/musicapp_2025exam/DetailedView.kt
+++ app/src/main/java/com/example/musicapp_2025exam/DetailedView.kt
@@ -6,8 +6,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,8 +23,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.musicapp_2025exam.ui.theme.MusicApp_2025ExamTheme
 
 class DetailedView : ComponentActivity() {
@@ -39,29 +47,13 @@ class DetailedView : ComponentActivity() {
             Column (
                 modifier = Modifier.fillMaxSize().padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally
             ){
-                Text(text = " Your Playlist ")
+                Text(text = " Your Playlist ", fontSize = 20.sp, fontFamily = FontFamily.SansSerif)
 
+                Button(onClick = { showsongs = !showsongs }) {
+                    Text(if (showsongs)"Show songs with a rating above 2 " else " Show all" )
+                }
 
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                Spacer(modifier = Modifier.height(16.dp))
 
 
 
@@ -77,14 +69,8 @@ class DetailedView : ComponentActivity() {
                 }) {
                     Text("Back to Home Screen")
                 }
-
-
-
-
-
-
-
             }
         }
-            }
-        }
+
+    }
+}
